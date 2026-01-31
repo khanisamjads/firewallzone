c138419c74f5249b77a39865b773ce578971a840
khanisamjads <44154054+khanisamjads@users.noreply.github.com>
2026-01-30 21:38:44 -0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -36,7 +36,7 @@ const slides = [
     title: 'Global Certifications',
     bgGradient: 'from-neutral-900 to-stone-900',
     bgImage: '/gallery/Slide 5.jpg',
-    icon: FaCertificate,
+    icon: Fa-Certificate,
     highlight: 'Cisco Official'
   },
   {
