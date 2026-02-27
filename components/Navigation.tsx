760d116a357c632fcf6005c335a985eaed49a3b5
khanisamjads <44154054+khanisamjads@users.noreply.github.com>
2026-02-27 10:20:51 -0600

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -91,7 +91,7 @@ export default function Navigation() {
                 <div className="absolute inset-y-0 right-0 left-[34%] bg-white rounded-r-md" />
                 <div className="relative flex items-center justify-center">
                   <Image 
-                    src="firewall-zone.png" 
+                    src="/firewall-zone.png" 
                     alt="Firewall Zone" 
                     width={220} 
                     height={48}
